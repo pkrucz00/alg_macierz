cee8474685c827ebf0f3ac99db8f2f4f62b03a44
pkrucz00 <pawel.kruczkiewicz@gmail.com>
2021-10-09 22:03:24 +0200

    Added multiplication.py and block_mul is under construyction

---
+++ zadanie1/py_scripts/block_mul.py
@@ -0,0 +1,6 @@
+# import numpy as np
+#
+# def block_mul(A, B, m_bs, n_bs, p_ps):
+#     C = np.zeros(np.size(A, 0), np.size(B, 1))
+#     for ii in range(m_bs):
+#
--- zadanie1/py_scripts/multiplication.py
+++ zadanie1/py_scripts/multiplication.py
@@ -1,13 +1,29 @@
 import numpy as np
 
-def mat_mul(A, B, mul_func):
-    C = np.zeros(np.size(A, 0), np.size(B, 1))
-    mul_func(A, B, C)
-    return C
 
 def multi_ijp(A, B, C):
     m, n, k = np.size(C, 0), np.size(C, 1), np.size(A, 1)
     for i in range(m):
         for j in range(n):
             for p in range(k):
-                C[i][j] = A[i][p] * B[p][j]
+                C[i][j] += A[i][p] * B[p][j]
+
+
+def mat_mul(A, B, mul_func=multi_ijp):
+    C = np.zeros((np.size(A, 0), np.size(B, 1)))
+    mul_func(A, B, C)
+    return C
+
+
+A = np.array(
+    [[1, 2, 2],
+     [2, 4, 3]])
+B = np.array(
+    [[2, 2, 2, 1],
+     [2, 3, 1, 4],
+     [2, 1, 3, 7]])
+C = mat_mul(A, B)
+print(C)
+
+C_expected = A @ B
+print(C_expected)
